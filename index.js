f0486e09adf9a80ec6078316ced0f7e42e9567b8
tga55a <tgardner2@my.waketech.edu>
2025-02-10 13:41:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+let words = ["i", "like", "to", "code"]
+const button = document.getElementById("generateBtn")
 
+function generateSentence() {
+    let sentence = ""
+    const element = document.getElementById("output")
+
+    for(let word of words) {
+        sentence = sentence + " " + word
+    }
+
+    element.innerHTML = sentence
+}
+button.addEventListener("click", generateSentence)
